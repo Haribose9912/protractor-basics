0ab8f5044dc63c616147722d75058c9b5214bcdf
Dumidu Handakumbura <DumiduH@99x.lk>
2018-07-16 08:59:11 +0530

    fixes config file.

--- configuration.js
+++ configuration.js
@@ -1,7 +1,7 @@
 var Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter');
 exports.config = {
 		seleniumAddress: "http://localhost:4444/wd/hub",
-		specs: [,"Testcases/LoginTestCase.js","Testcases/WithdrawlTestCase.js","Testcases/TransactionsTestCase.js","Testcases/NewUserFlowTestCase.js","Testcases/DepositTestCase.js","Testcases/CustomerManagementTestCase.js","Testcases/AccountTestCase.js"],
+		specs: ["Testcases/LoginTestCase.js","Testcases/WithdrawlTestCase.js","Testcases/TransactionsTestCase.js","Testcases/NewUserFlowTestCase.js","Testcases/DepositTestCase.js","Testcases/CustomerManagementTestCase.js","Testcases/AccountTestCase.js"],
 	
 	onPrepare : function(){
 	console.log('--- starting testcase execution for the way2automation backing example site ---');
